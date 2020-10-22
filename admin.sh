187a5e52a7da1e1c005f2de94d1a8a0f96bb7a39
pmarcelojr <willian.fcamara@fornecedores.klabin.com.br>
2020-10-22 11:47:02 -0300

    funcao para atualizar lista de pacotes

--- admin.sh
+++ admin.sh
@@ -3,14 +3,16 @@ clear
 
 main(){
     echo "[ 1 ]  Adicionar Usuário"
-    echo "[ 2 ] Sair"
+    echo "[ 2 ] Atualizar a lista de pacotes"
+    echo "[ 3 ] Sair"
     echo
     echo -n "Qual a opcao desejada ? "
     read OPCAO
     case $OPCAO in
         1) adicionar ;;
-        2) exit ;;
-        *) "Opcao desconhecida." ; echo ; Principal ;;
+        2) atualizar ;;
+        3) exit ;;
+        *) "Opcao desconhecida." ; echo ; main ;;
     esac
 }
 
@@ -20,6 +22,12 @@ adicionar(){
     useradd $USUARIO
     echo "Informe a senha para o novo usuario"
     passwd $USUARIO
+        main
+}
+
+atualizar(){
+    echo "Atualizando a lista de pacotes do sistema"
+    sudo apt update
         main
 }
 
